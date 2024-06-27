5aff3c192348e176ff13f9c5a3e15e5ef3981806
Demnlight <69733532+Demnlight@users.noreply.github.com>
2024-06-27 21:41:50 +0300

    [Fixed merge issue]

--- Assets/CPlayer/PlayerMovement.cs
+++ Assets/CPlayer/PlayerMovement.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class playerMovement : MonoBehaviour {
-    [SerializeField] private float Accelaration_Player = 50f;
-    [SerializeField] private float Deccelaration_Player = 50f;
-    [SerializeField] private float Max_Speed = 250f;
+    [SerializeField] private float Accelaration_Player = 5f;
+    [SerializeField] private float Deccelaration_Player = 5f;
+    [SerializeField] private float Max_Speed = 20f;
 
     private Rigidbody2D rigibody;
 
     private void Awake( ) {
         rigibody = GetComponent<Rigidbody2D>( );
+    }
+    // Start is called before the first frame update
+    void Start( ) {
+
+    }
+
+    // Update is called once per frame
+    void Update( ) {
+
     }
 
     private void FixedUpdate( ) {
@@ -29,17 +35,7 @@ public class playerMovement : MonoBehaviour {
             if (rigibody.velocity.magnitude > Max_Speed) {
                 rigibody.velocity = rigibody.velocity.normalized * Max_Speed;
             }
-        Vector2 inputVector = new Vector2( flAxisRawHorizontal, flAxisRawVertical );
-
-        if (inputVector != Vector2.zero) {
-            rigibody.AddForce( inputVector * Accelaration_Player, ForceMode2D.Force );
-
-            if (rigibody.velocity.magnitude > Max_Speed) {
-                rigibody.velocity = rigibody.velocity.normalized * Max_Speed;
-            }
         } else {
-            rigibody.AddForce( rigibody.velocity * -Deccelaration_Player, ForceMode2D.Force );
-        }
             rigibody.AddForce( rigibody.velocity * -Deccelaration_Player, ForceMode2D.Force );
         }
     }
