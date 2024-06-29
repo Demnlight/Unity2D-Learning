88dd7481314168c8bc5a11614d172bc8d93bf131
Demnlight <69733532+Demnlight@users.noreply.github.com>
2024-06-29 23:21:57 +0300

    [+] Added exception

--- Assets/BaseEntity/Player/PlayerMovement.cs
+++ Assets/BaseEntity/Player/PlayerMovement.cs
@@ -1,10 +1,11 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent( typeof( Rigidbody2D ) )]
 public class playerMovement : MonoBehaviour {
-    [SerializeField, Range( 0, 9999)] private float maxSpeed = 250f;
-    [SerializeField, Range( 0, 0.99f)] private float friction = 0.9f;
+    [SerializeField, Range( 0, 9999 )] private float maxSpeed = 250f;
+    [SerializeField, Range( 0, 0.99f )] private float friction = 0.9f;
 
     private Rigidbody2D rb;
     private PlayerInputActions inputActions;
@@ -18,6 +19,9 @@ public class playerMovement : MonoBehaviour {
     }
 
     private void FixedUpdate( ) {
+        if (inputActions == null)
+            throw new ArgumentException("inputActions == null");
+
         vMovingDirection = inputActions.Player.Move.ReadValue<Vector2>( );
 
         Vector2 targetVelocity = vMovingDirection * maxSpeed;
