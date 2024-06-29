9b249564606208199d3426a5995adff2e1dbabaf
Demnlight <69733532+Demnlight@users.noreply.github.com>
2024-06-29 17:26:44 +0300

    [!] Update [+] Removed unused parametr "acceleration"

--- Assets/BaseEntity/Player/PlayerMovement.cs
+++ Assets/BaseEntity/Player/PlayerMovement.cs
@@ -3,12 +3,10 @@ using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour {
     [SerializeField] private float maxSpeed = 250f;
-    [SerializeField] private float acceleration = 5f;
     [SerializeField] private float friction = 0.9f;
 
     private Rigidbody2D rb;
     private PlayerInputActions inputActions;
-    private Vector2 Last_Direction;
     private Vector2 vMovingDirection;
 
     void Awake( ) {
@@ -20,27 +18,6 @@ public class playerMovement : MonoBehaviour {
 
     private void FixedUpdate( ) {
         vMovingDirection = inputActions.Player.Move.ReadValue<Vector2>( );
-        /*if (vMovingDirection != Vector2.zero) {
-            if (vMovingDirection != Last_Direction) {
-                rigibody.velocity = Vector2.zero;
-                Last_Direction = vMovingDirection;
-            }
-
-            rigibody.velocity += vMovingDirection * Accelaration * Time.deltaTime;
-
-            if (rigibody.velocity.magnitude > Max_Speed) {
-                rigibody.velocity = rigibody.velocity.normalized * Max_Speed;
-            }
-        } else {
-            rigibody.velocity -= rigibody.velocity.normalized * Deccelaration * Time.deltaTime;
-
-            if (rigibody.velocity.magnitude < 0.1f) {
-                rigibody.velocity = Vector2.zero;
-            }
-        }
-
-        if (vMovingDirection.x != 0)
-            transform.localScale = new Vector3( vMovingDirection.x, transform.localScale.y, transform.localScale.z );*/
 
         Vector2 targetVelocity = vMovingDirection * maxSpeed;
         Vector2 speedDiff = targetVelocity - rb.velocity;
